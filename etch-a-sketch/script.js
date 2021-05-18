82ab3143aa3d75bd4ab18a0fa117ee9923b9ac96
tylerBrittain42 <tbrittain100@gmail.com>
2021-05-17 22:31:06 -0700

    initial tileboard created

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,1 +1,13 @@
-console.log("yes")
+createBoard();
+
+function createBoard(){
+
+    const board = document.getElementById("board-container")
+
+    for(let i = 0; i < (16*16); i++){
+        const tag = document.createElement("div");
+        tag.classList.add("board-tile")
+        board.appendChild(tag);
+    }
+
+}
