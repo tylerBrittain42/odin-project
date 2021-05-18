e99eef5c008b5145216e21e22b1170eb1cb36742
tylerBrittain42 <tbrittain100@gmail.com>
2021-05-17 22:50:13 -0700

    mouseover event listeners added

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -2,12 +2,27 @@ createBoard();
 
 function createBoard(){
 
+    //Grabs the container
     const board = document.getElementById("board-container")
 
+    //creates the tiles inside of the board
     for(let i = 0; i < (16*16); i++){
-        const tag = document.createElement("div");
+        const tag = document.createElement("button");
         tag.classList.add("board-tile")
+        tag.setAttribute('id',i)
         board.appendChild(tag);
     }
+
+    //adding event listeners to all of the buttons
+    const tiles = document.querySelectorAll('.board-tile');
+    console.log(tiles)
+    tiles.forEach((tile) => {
+        tile.addEventListener("mouseover", () => {
+            console.log(tile.id);
+            tile.style.backgroundColor = "red"
+
+        })
+
+    })
 
 }
