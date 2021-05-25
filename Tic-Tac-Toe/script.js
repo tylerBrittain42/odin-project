11d4280207d82a41f6ddd155a5e014ac31b465dc
tylerBrittain42 <tbrittain100@gmail.com>
2021-05-24 17:10:47 -0700

    added empty factory and module funtions

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,1 +1,53 @@
-console.log('yes')
+// IIIFE example
+const gameBoard = (() => {
+
+    let boardLayout = [1]
+
+    const getLayout = () => boardLayout
+
+    return{
+
+        getLayout
+    }
+
+
+})()
+
+// Factory example
+const Player = () => {
+
+
+
+
+}
+
+
+
+
+createListeners()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createListeners(){
+
+    const tiles = document.querySelectorAll('.grid-item')
+    tiles.forEach((tile) => {
+        tile.addEventListener('click', () => {
+            tile.innerHTML = (tile.innerHTML === 'X') ? 'O' : 'X'
+        })
+
+    })
+
+}
