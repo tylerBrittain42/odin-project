e89ec5fa0021097438a99f2567b223d9f9c4db68
tylerBrittain42 <tbrittain100@gmail.com>
2021-05-27 17:37:05 -0700

    used prettier to format

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,9 +1,9 @@
 /*eslint linebreak-style: ["error", "windows"]*/
 // IIIFE example
 const gameBoard = (() => {
-  const boardLayout = []
+  const boardLayout = [];
   for (let i = 0; i < 9; i++) {
-    boardLayout[i] = 'e'
+    boardLayout[i] = "e";
   }
 
   const getLayout = () => boardLayout;
@@ -14,9 +14,10 @@ const gameBoard = (() => {
       if (
         boardLayout[i] === boardLayout[i + 1] &&
         boardLayout[i + 1] === boardLayout[i + 2] &&
-        boardLayout[i] !== 'e'
-      )
-        {return true;}
+        boardLayout[i] !== "e"
+      ) {
+        return true;
+      }
     }
 
     // Vertical
@@ -24,31 +25,33 @@ const gameBoard = (() => {
       if (
         boardLayout[i] === boardLayout[i + 3] &&
         boardLayout[i + 3] === boardLayout[i + 6] &&
-        boardLayout[i] !== 'e'
-      )
-        {return true;}
+        boardLayout[i] !== "e"
+      ) {
+        return true;
+      }
     }
 
     // Diagonal
     if (
       boardLayout[0] === boardLayout[4] &&
       boardLayout[4] === boardLayout[8] &&
-      boardLayout[0] !== 'e'
-    )
-      {return true;}
-    else if (
+      boardLayout[0] !== "e"
+    ) {
+      return true;
+    } else if (
       boardLayout[2] === boardLayout[4] &&
       boardLayout[4] === boardLayout[6] &&
-      boardLayout[2] !== 'e'
-    )
-      {return true;}
+      boardLayout[2] !== "e"
+    ) {
+      return true;
+    }
   };
 
   const isTie = () => {
     let tie = true;
 
     boardLayout.forEach((tile) => {
-      if (tile === 'e') {
+      if (tile === "e") {
         tie = false;
       }
     });
@@ -56,7 +59,7 @@ const gameBoard = (() => {
   };
 
   const isAvailable = (currentTile) => {
-    if (boardLayout[currentTile.id] === 'e') return true;
+    if (boardLayout[currentTile.id] === "e") return true;
     else return false;
   };
 
@@ -76,10 +79,10 @@ const gameBoard = (() => {
 
 const controller = (() => {
   const createListeners = () => {
-    const tiles = document.querySelectorAll('.grid-item');
-    const header = document.querySelector('h1');
+    const tiles = document.querySelectorAll(".grid-item");
+    const header = document.querySelector("h1");
     tiles.forEach((tile) => {
-      tile.addEventListener('click', () => {
+      tile.addEventListener("click", () => {
         // valid move
         if (gameBoard.isAvailable(tile)) {
           gameBoard.claimSpace(
@@ -92,9 +95,9 @@ const controller = (() => {
             header.innerHTML =
               (turnCount % 2 === 0
                 ? playerOne.getToken()
-                : playerTwo.getToken()) + ' won';
+                : playerTwo.getToken()) + " won";
           } else if (gameBoard.isTie()) {
-            header.innerHTML = 'Tie game';
+            header.innerHTML = "Tie game";
           }
           turnCount++;
         }
@@ -121,7 +124,7 @@ const Player = (token) => {
 };
 
 let turnCount = 0;
-const playerOne = Player('X');
-const playerTwo = Player('O');
+const playerOne = Player("X");
+const playerTwo = Player("O");
 console.log(playerOne.getToken());
 controller.createListeners();
