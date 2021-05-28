8064b36484147dbfe67eb8a47110392e8ca4d6a9
tylerBrittain42 <tbrittain100@gmail.com>
2021-05-28 15:55:06 -0700

    added modules to example

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,17 @@
 import _ from 'lodash';
+import myName from './myName.js';
+
 
 function component() {
     const element = document.createElement('div');
   
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', '. ', myName('Chuck')], ' ');
   
     return element;
   }
   
   document.body.appendChild(component());
+
+
+  
