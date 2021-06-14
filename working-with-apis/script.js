9f1261d76574a2bbedb89dfd36a85021ebd1406e
tylerBrittain42 <tbrittain100@gmail.com>
2021-06-13 23:18:36 -0700

    removed unnecc file

--- working-with-apis/script.js
+++
@@ -1,5 +0,0 @@
-//https://api.giphy.com/v1/gifs/translate?api_key=gkoPri4tPwq2UF4bLLdDXZwnlsLXcdpm&s=cats
-
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=gkoPri4tPwq2UF4bLLdDXZwnlsLXcdpm&s=cats',{
-    mode:'cors'
-})
