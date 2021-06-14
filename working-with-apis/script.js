ecc5482e9b19de4541fbd355cc8b6b9c442db7db
tylerBrittain42 <tbrittain100@gmail.com>
2021-06-13 23:17:43 -0700

    added API example using fetch

---
+++ working-with-apis/script.js
@@ -0,0 +1,5 @@
+//https://api.giphy.com/v1/gifs/translate?api_key=gkoPri4tPwq2UF4bLLdDXZwnlsLXcdpm&s=cats
+
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=gkoPri4tPwq2UF4bLLdDXZwnlsLXcdpm&s=cats',{
+    mode:'cors'
+})
