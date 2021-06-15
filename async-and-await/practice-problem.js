41a26e55a6566509af375ad863690fa3b3789575
tylerBrittain42 <tbrittain100@gmail.com>
2021-06-14 19:02:03 -0700

    added practice problem

---
+++ async-and-await/practice-problem.js
@@ -0,0 +1,29 @@
+// Converted the following function to utilize async await
+
+function loadJson(url) {
+    return fetch(url)
+      .then(response => {
+        if (response.status == 200) {
+          return response.json();
+        } else {
+          throw new Error(response.status);
+        }
+      });
+  }
+  
+  loadJson('no-such-user.json')
+    .catch(alert); // Error: 404
+
+// SOLUTION 
+async function loadJson(url) {
+    const res = await fetch(url)
+    if(res.status === 200){
+        const json = await res.json()
+        return json}
+    else
+          throw new Error(response.status);
+      };
+  
+  loadJson('no-such-user.json')
+    .catch(alert); // Error: 404
+
