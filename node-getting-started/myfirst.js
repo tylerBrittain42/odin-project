54cf099b766d05d5e96c3567acece1eb2e2e1a55
tylerBrittain42 <tbrittain100@gmail.com>
2021-06-22 00:06:16 -0700

    added node tutorials from w3schools

---
+++ node-getting-started/myfirst.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var dateModule = require('./myfirstmodule')
+
+
+http.createServer(function (req,res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello World')
+    res.write(req.url)
+    res.end()
+}).listen(8080);
+
