2da941b731650e470880f7f36f492ba27d9e266a
tylerBrittain42 <tbrittain100@gmail.com>
2021-06-22 20:29:34 -0700

    completed node-intro-project; also updated ignore file

--- node-intro-project/app.js
+++ node-intro-project/app.js
@@ -4,28 +4,28 @@ const url = require('url')
 
 http.createServer((req, res) => {
 
-    const desiredPath = url.parse(req.url).pathname.substring(1) + '.html'
+    // sets desiredPath to index in cases where no path is specified
+    const desiredPath = (url.parse(req.url).pathname === "/") ? 'index.html': url.parse(req.url).pathname.substring(1) + '.html'
 
     // Without this: will try and load the favicon file
     if (desiredPath === 'favicon.ico.html')
         return
 
-    console.log(desiredPath)
-
     res.writeHead(200, {'Content-Type': 'text/html'})
     fs.readFile(desiredPath, (e, data) => {
         if(e){
-            console.log(e)
+            // console.log(e)
             fs.readFile('404.html', (e, data) => {
                 res.write(data)
                 return res.end()
             })
         }
         else{
-            
             res.write(data)
             return res.end()
         }
     })
 
-}).listen(8080)
+}).listen(8080, () => {
+    console.log("listening on port 8080")
+})
