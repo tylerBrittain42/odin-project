f7de00bd18545838d21a83c5f90b17865b10061c
tylerBrittain42 <tbrittain100@gmail.com>
2021-06-26 16:08:03 -0700

    added models

--- express-101/app.js
+++ express-101/app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Setting up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://127.0.0.1:27017/library'
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
