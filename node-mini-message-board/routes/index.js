a553df958ed0bb66c92039560eac10e10c73be76
tylerBrittain42 <tbrittain100@gmail.com>
2021-06-28 18:35:00 -0700

    base template added for new entries

--- node-mini-message-board/routes/index.js
+++ node-mini-message-board/routes/index.js
@@ -1,9 +1,30 @@
+const messages = [
+  {
+    text: "Hello",
+    user: "Chuck",
+    added: new Date()
+  },
+  {
+    text: "Hola",
+    user: "Chris",
+    added: new Date()
+  },
+  
+]
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.render('index', { title: 'Mini MessageBoard', messages: messages  });
 });
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+
 
 module.exports = router;
