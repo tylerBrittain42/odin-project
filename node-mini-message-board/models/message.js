0045cf160d13e3316da077a589051b26cd41d057
tylerBrittain42 <tbrittain100@gmail.com>
2021-06-30 17:21:13 -0700

    added model for mongo

---
+++ node-mini-message-board/models/message.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema(
+    {
+        message: {type: String, required: true},
+        author: {type: String, required: true},
+        date: {type:Date, default: Date.now}
+    });
+
+module.exports = mongoose.model('Message', messageSchema)
