0d19bb1854726c676d444c3ce8f1d8044f152606
tylerBrittain42 <tbrittain100@gmail.com>
2021-07-06 15:29:45 -0700

    completed system schema

---
+++ node-inventory-application/models/system.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const SystemSchema = new Schema(
+    {
+        name: {type: String, required: true},
+        price: {type: Number, required: true},
+    }
+);
+
+SystemSchema
+.virtual('url')
+.get(() => {
+    return('/catalog/system/' + this._id)
+});
+
+module.exports = mongoose.model('System', SystemSchema);
